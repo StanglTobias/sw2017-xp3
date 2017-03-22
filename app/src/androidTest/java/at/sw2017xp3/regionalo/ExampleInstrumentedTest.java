b97fa2dabc4a87e756ccb16f9c3053f6186f92f4
Kevin Triebnig <Kevin Triebnig>
2017-03-22 14:22:05 +0100

    added test for searchfield and menubuttons

--- app/src/androidTest/java/at/sw2017xp3/regionalo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/ExampleInstrumentedTest.java
@@ -2,11 +2,22 @@ package at.sw2017xp3.regionalo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.Display;
+import android.view.WindowManager;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +33,30 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("at.sw2017xp3.regionalo", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<HomeActivity> menuActivityTestRule =
+            new ActivityTestRule<>(HomeActivity.class, true, true);
+
+    @Test
+    public void testMenuButtonsFirstScreen () throws Exception {
+        onView(withText("Fleisch")).perform(click());
+        onView(withText("Gemüse")).perform(click());
+        onView(withText("Getreide")).perform(click());
+        onView(withText("Obst")).perform(click());
+        onView(withText("Milch")).perform(click());
+        onView(withId(R.id.BtnSonstiges)).perform(scrollTo(), click());
+    }
+
+    @Test
+    public void testSearchFunction() {
+        onView(withId(R.id.searchView)).perform(click());
+        onView(withId(R.id.searchView)).perform(typeText("Hallo"), click());
+    }
+
+    @Test
+    public void testRecommendedPictures (){
+
     }
 }
