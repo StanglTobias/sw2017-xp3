b484a7471c19a62e7a65299d867458bb3a2e0bd4
YSTC <steinkellnerchristoph@gmx.at>
2017-03-29 16:25:09 +0200

    Added sql script and php script to retrieve single users

---
+++ php/config.php
@@ -0,0 +1,12 @@
+db_config.php
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "sw-ma-xp3"); // db user
+define('DB_PASSWORD', "sw2017"); // db password (mention your db password here)
+define('DB_DATABASE', "sw-ma-xp3"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
