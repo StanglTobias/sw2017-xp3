70b73583bd0339d81404eca4042f6517ce20c963
Lukas Holder <lukas.holder@student.tugraz.at>
2017-04-05 14:57:56 +0200

    test added

--- app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
+++ app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
@@ -2,6 +2,7 @@ package at.sw2017xp3.regionalo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 /**
  * Created by Christof on 05.04.2017.
@@ -13,5 +14,12 @@ public class ProductDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detailes);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.loginbutton, menu);
+        return true;
     }
 }
