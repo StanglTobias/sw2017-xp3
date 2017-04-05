258b8f844bfbb466ce12006d8e183bed944d95e1
Matthias Almer <matthias.almer@student.tugraz.at>
2017-04-05 11:22:34 +0200

    began with tests

--- app/src/androidTest/java/at/sw2017xp3/regionalo/ExtendedSearchActivityTest.java
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/ExtendedSearchActivityTest.java
@@ -10,6 +10,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4.class)
@@ -22,6 +28,41 @@ public class ExtendedSearchActivityTest {
     }
 
     @Rule
-    public ActivityTestRule<ExtendedSearchActivity> menuActivityTestRule =
-            new ActivityTestRule<>(ExtendedSearchActivity.class, true, true);
+    public ActivityTestRule<ExtendedSearchActivity> extendedSearchActivityActivityTestRule =
+            new ActivityTestRule<>(ExtendedSearchActivity.class);
+
+
+    @Test
+    public void testButtons() {
+        onView(withId(R.id.Button_ID_ExtendedSearchStart)).perform(click());
+        onView(withId(R.id.Button_ID_ResetFilterExtendedSearch)).perform(click());
+    }
+
+    @Test
+    public void testFields() {
+        onView(withId(R.id.Spinner_ID_ExtendedSearch)).perform(click());
+        onView(withText("Alphabetisch")).perform(click());
+
+        onView(withId(R.id.checkBox_ID_BiologischerAnbau)).perform(click());
+
+        onView(withId(R.id.checkBox_ID_KategorieObst)).perform(click());
+        onView(withId(R.id.checkBox_ID_KategorieGemüse)).perform(click());
+        onView(withId(R.id.checkBox_ID_KategoriePilze)).perform(click());
+        onView(withId(R.id.checkBox_ID_KategoriePlanzenUndSamen)).perform(click());
+        onView(withId(R.id.checkBox_ID_KategorieHolz)).perform(click());
+        onView(withId(R.id.checkBox_ID_WeitereGartenprodukte)).perform(click());
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
