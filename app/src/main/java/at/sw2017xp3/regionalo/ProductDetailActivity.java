c0b9034a8807ae4cabdc6b3c2db7a0fb606ac037
Christof Jahn <christof.jahn@student.tugraz.at>
2017-04-05 14:17:52 +0200

    new java class

---
+++ app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
@@ -0,0 +1,17 @@
+package at.sw2017xp3.regionalo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Christof on 05.04.2017.
+ */
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
