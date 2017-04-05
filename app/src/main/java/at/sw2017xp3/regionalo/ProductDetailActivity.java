2e7dee75f8b7c7b98ed94d1dadded983d57aabc1
Christof Jahn <christof.jahn@student.tugraz.at>
2017-04-05 14:19:01 +0200

    error corrected

--- app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
+++ app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
@@ -12,6 +12,6 @@ public class ProductDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_product_detailes);
     }
 }
