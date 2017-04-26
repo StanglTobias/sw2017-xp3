d5f77b3d96083ef1cd542ba891ca5de62ad3dc5a
kevintu95 <k.triebnig@student.tugraz.at>
2017-04-26 08:31:17 +0200

    added database connection

--- app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
+++ app/src/main/java/at/sw2017xp3/regionalo/ProductDetailActivity.java
@@ -1,22 +1,95 @@
 package at.sw2017xp3.regionalo;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import at.sw2017xp3.regionalo.model.Product;
+import at.sw2017xp3.regionalo.util.HttpUtils;
+import at.sw2017xp3.regionalo.util.JsonObjectMapper;
 
 /**
  * Created by Christof on 05.04.2017.
  */
 
-public class ProductDetailActivity extends AppCompatActivity {
+public class ProductDetailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ArrayList<View> list_of_elements = new ArrayList<>();
+    private int like_button_counter_;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detailes);
+
+        list_of_elements.addAll(Arrays.asList(
+                findViewById(R.id.buttonLike),
+                findViewById(R.id.ButtonContact)));
+
+        for (int i = 0; i < list_of_elements.size(); i++) {
+            list_of_elements.get(i).setOnClickListener(this);
+        }
+
+        Uri uri = Uri.parse("http://sw-ma-xp3.bplaced.net/MySQLadmin/product.php")
+         .buildUpon()
+         .appendQueryParameter("id", "1").build();
+
+        new GetProductTask().execute(uri.toString());
     }
+
+    private class GetProductTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return downloadContent(params[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve data. URL may be invalid.";
+            }
+        }
+    }
+
+    private String downloadContent(String myurl) throws IOException {
+        InputStream is = null;
+        int length = 10000;
+
+        try {
+            HttpURLConnection conn = HttpUtils.httpGet(myurl);
+
+            String contentAsString = HttpUtils.convertInputStreamToString(conn.getInputStream(), length);
+            JSONArray arr = new JSONArray(contentAsString); //featured products
+            JSONObject mJsonObject = arr.getJSONObject(0);//one product
+
+            String allProductNames;
+
+            Product p =  JsonObjectMapper.CreateProduct(mJsonObject);
+
+            return p.getName();
+        } catch (Exception ex) {
+            return "";
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,5 +112,22 @@ public class ProductDetailActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Button clickedButton = (Button)v;
+
+        switch (clickedButton.getId()){
+            case R.id.buttonLike:
+              like_button_counter_ =  Integer.valueOf((String)(((TextView)findViewById(R.id.textViewLikeCount)).getText()));
+              like_button_counter_++;
+              ((TextView)findViewById(R.id.textViewLikeCount)).setText(Integer.toString(like_button_counter_));
+              break;
+
+            case R.id.ButtonContact:
+                //onClick moveTo Website or show contact data
+                break;
+        }
     }
 }
