a573cdff6685fb121a3814b662cfd6be1a0bbffd
kevintu95 <k.triebnig@student.tugraz.at>
2017-04-26 14:08:40 +0200

    [LH, KT] Added Unit tests

--- app/src/test/java/at/sw2017xp3/regionalo/ProductDetailUnitTest.java
+++ app/src/test/java/at/sw2017xp3/regionalo/ProductDetailUnitTest.java
@@ -11,9 +11,44 @@ import static junit.framework.Assert.assertEquals;
 
 public class ProductDetailUnitTest {
     @Test
-    public void isBioTest(){
+    public void isBioTestForTrue() {
+        String bio = ProductDetailActivity.isBio(true);
+        assertEquals("Ja", bio);
+    }
 
-     String bio = ProductDetailActivity.isBio(true);
-    assertEquals("Ja",bio);
+    @Test
+    public void isBioTestForFalse() {
+        String bio = ProductDetailActivity.isBio(false);
+        assertEquals("Nein", bio);
+    }
+
+    @Test
+    public void testCategorieFleisch() {
+        assertEquals("Fleisch", ProductDetailActivity.productCategorieName(1));
+    }
+
+    @Test
+    public void testCategorieObst() {
+        assertEquals("Obst", ProductDetailActivity.productCategorieName(2));
+    }
+
+    @Test
+    public void testCategorieGemüuese() {
+        assertEquals(ProductDetailActivity.productCategorieName(3), "Gemüse");
+    }
+
+    @Test
+    public void testCategorieMilchprodukte() {
+        assertEquals("Milchprodukte", ProductDetailActivity.productCategorieName(4));
+    }
+
+    @Test
+    public void testCategorieGetreide() {
+        assertEquals("Getreide", ProductDetailActivity.productCategorieName(5));
+    }
+
+    @Test
+    public void testCategorieSonstiges() {
+        assertEquals("Sonstiges", ProductDetailActivity.productCategorieName(6));
     }
 }
