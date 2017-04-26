865ff7918660e4aeb8a4fd9c081d35b7440ac1b8
Lukas Holder <lukas.holder@gmx.at>
2017-04-26 14:07:09 +0200

    [LH,KT] added first test

--- app/src/test/java/at/sw2017xp3/regionalo/ProductDetailUnitTest.java
+++ app/src/test/java/at/sw2017xp3/regionalo/ProductDetailUnitTest.java
@@ -1,9 +1,19 @@
 package at.sw2017xp3.regionalo;
 
+import org.junit.Test;
+
+import static java.lang.Boolean.TRUE;
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by Kevin on 26.04.2017.
  */
 
 public class ProductDetailUnitTest {
+    @Test
+    public void isBioTest(){
 
+     String bio = ProductDetailActivity.isBio(true);
+    assertEquals("Ja",bio);
+    }
 }
