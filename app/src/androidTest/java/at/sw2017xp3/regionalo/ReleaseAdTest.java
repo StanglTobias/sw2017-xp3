5fb9fe9fc9d9ae200c3941e319404b8e0e405af9
Matthias Almer <matthias.almer@student.tugraz.at>
2017-04-26 09:51:32 +0200

    tests

--- app/src/androidTest/java/at/sw2017xp3/regionalo/ReleaseAdTest.java
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/ReleaseAdTest.java
@@ -1,8 +1,83 @@
 package at.sw2017xp3.regionalo;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+
 /**
- * Created by Anna on 26.04.2017.
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 
+
+@RunWith(AndroidJUnit4.class)
 public class ReleaseAdTest {
+
+    @Rule
+    public ActivityTestRule<ReleaseAdActivity> mActivityRule = new
+            ActivityTestRule<>(ReleaseAdActivity.class);
+
+
+    @Test
+    public void checkTextViews() {
+
+        onView(withId(R.id.TextView_ID_ReleaseAdName)).perform(scrollTo(), typeText("Speck"));
+        onView(withId(R.id.TextView_ID_ReleaseAdName)).check(matches(withText("Speck")));
+
+        onView(withId(R.id.radioButton_ID_Privat)).perform(scrollTo(), click());
+        onView(withId(R.id.radioButton_ID_Firma)).check(matches(not(isChecked())));
+        onView(withId(R.id.radioButton_ID_Privat)).check(matches((isChecked())));
+
+        onView(withId(R.id.editText_ID_Beschreibung)).perform(scrollTo(), typeText("ein seeeeeeeeeeeeeeeeeeeehr langer Text"));
+        onView(withId(R.id.editText_ID_Beschreibung)).check(matches(withText("ein seeeeeeeeeeeeeeeeeeeehr langer Text")));
+
+        onView(withId(R.id.spinner_ID_ReleaseAdKategorie)).perform(scrollTo(), click());
+        onData(allOf(is(instanceOf(String.class)), is("Kategorie 2"))).perform(click());
+        onView(withId(R.id.spinner_ID_ReleaseAdKategorie)).check(matches(withSpinnerText(containsString("Kategorie 2"))));
+
+        onView(withId(R.id.spinner_ID_ReleaseAdUnterkategorie)).perform(scrollTo(), click());
+        onData(allOf(is(instanceOf(String.class)), is("Unterkategorie 2"))).perform(click());
+        onView(withId(R.id.spinner_ID_ReleaseAdUnterkategorie)).check(matches(withSpinnerText(containsString("Unterkategorie 2"))));
+
+
+
+
+
+
+
+
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
 }
--- app/src/main/java/at/sw2017xp3/regionalo/ReleaseAdActivity.java
+++ app/src/main/java/at/sw2017xp3/regionalo/ReleaseAdActivity.java
@@ -1,8 +1,71 @@
-package at.sw2017xp3.regionalo.util;
+package at.sw2017xp3.regionalo;
 
-/**
- * Created by Anna on 26.04.2017.
- */
 
-public class ReleaseAdActivity {
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+
+import at.sw2017xp3.regionalo.LoginActivity;
+import at.sw2017xp3.regionalo.R;
+
+public class ReleaseAdActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ArrayList<View> list_of_buttons = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_release_ad);
+
+        addButtonListeners();
+        findViewById(R.id.textView_ID_DateVon).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              //  datePickerDialog.show();
+            }
+        });
+    }
+
+    private void addButtonListeners()
+    {
+        list_of_buttons.addAll(Arrays.asList(
+                findViewById(R.id.button_ID_Bildauswaehlen),
+                findViewById(R.id.button_ID_Bild2auswaehlen),
+                findViewById(R.id.button_ID_ReleaseAd_Abbrechen),
+                findViewById(R.id.button_ID_ReleaseAdSpeichern)));
+
+        for (int i = 0; i < list_of_buttons.size(); i++) {
+            list_of_buttons.get(i).setOnClickListener(this);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Button clickedButton = (Button) v;
+        switch (clickedButton.getId()) {
+            case R.id.button_ID_Bildauswaehlen:
+                break;
+            case R.id.button_ID_Bild2auswaehlen:
+                break;
+            case R.id.button_ID_ReleaseAd_Abbrechen:
+                break;
+            case R.id.button_ID_ReleaseAdSpeichern:
+                break;
+        }
+    }
 }
