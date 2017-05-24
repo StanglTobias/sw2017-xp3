d5dcedcb674791f472277dbad858dd9552cd825d
Lukas Holder <lukas.holder@gmx.at>
2017-05-24 12:02:28 +0200

    [LH] [KT] linked xml and java file

--- app/src/main/java/at/sw2017xp3/regionalo/ReleaseAdActivity.java
+++ app/src/main/java/at/sw2017xp3/regionalo/ReleaseAdActivity.java
@@ -1,8 +1,17 @@
 package at.sw2017xp3.regionalo;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by lukas on 24.05.2017.
  */
 
-public class ReleaseAdActivity {
+public class ReleaseAdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_release_ad);
+    }
 }
