b9062ed7ca2eb6b26fa2e41fbd3cf3c16fc0725d
Lukas Holder <lukas.holder@gmx.at>
2017-05-24 12:56:07 +0200

    [LH] [KT] added Tests, added the activitxy to the manifest, fixed the dropdownmenu

---
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/ReleaseAdInstrumentedTest.java
@@ -0,0 +1,45 @@
+package at.sw2017xp3.regionalo;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+/**
+ * Created by lukas on 24.05.2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class ReleaseAdInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<ReleaseAdActivity> mActivityRule = new
+            ActivityTestRule<>(ReleaseAdActivity.class);
+
+    @Test
+    public void checkTextClicks() {
+        onView(withId(R.id.TextView_ID_Angebotstitel)).perform(scrollTo(), typeText("Supertomaten"));
+        onView(withId(R.id.editText2)).perform(scrollTo(), typeText("Diese Tomaten sind super mega toll!!!"));
+        onView(withId(R.id.spinner_ID_ReleaseAdKategorie)).perform(scrollTo(),click());
+        pressBack();
+        onView(withId(R.id.button_ID_Bildauswaehlen)).perform(scrollTo(),click());
+        onView(withId(R.id.textView_ID_Preis)).perform(scrollTo(), typeText("1000"));
+        onView(withId(R.id.spinner_ID_Einheit)).perform(scrollTo(),click());
+        pressBack();
+        /*onView(withId(R.id.checkBox_ID_BiologischerAnbau)).perform(scrollTo(),click());
+        /*onView(withId(R.id.checkBox_ID_SelbstErnten)).perform(scrollTo(),click());
+        onView(withId(R.id.checkBox_ID_BereitsGeerntet)).perform(scrollTo(),click());*/
+        onView(withId(R.id.button_ID_ReleaseAd_Abbrechen)).perform(scrollTo(),click());
+        onView(withId(R.id.button_ID_ReleaseAdSpeichern)).perform(scrollTo(),click());
+    }
+}
