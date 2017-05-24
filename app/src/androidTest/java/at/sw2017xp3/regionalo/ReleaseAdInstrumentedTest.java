2a2cd7f0d4ef1e4f5b98eafe2031d4ef5feaa739
kevintu95 <k.triebnig@student.tugraz.at>
2017-05-24 15:59:14 +0200

    [LH, KT] Added test for RealeaseAdActivity

--- app/src/androidTest/java/at/sw2017xp3/regionalo/ReleaseAdInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/ReleaseAdInstrumentedTest.java
@@ -7,6 +7,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -23,11 +24,12 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 public class ReleaseAdInstrumentedTest {
 
     @Rule
-    public ActivityTestRule<ReleaseAdActivity> mActivityRule = new
-            ActivityTestRule<>(ReleaseAdActivity.class);
+    public ActivityTestRule<LoginActivity> mActivityRule = new
+            ActivityTestRule<>(LoginActivity.class);
 
     @Test
     public void checkTextClicks() {
+        moveToRightAcitivity();
         onView(withId(R.id.TextView_ID_Angebotstitel)).perform(scrollTo(), typeText("Supertomaten"));
         onView(withId(R.id.editText2)).perform(scrollTo(), typeText("Diese Tomaten sind super mega toll!!!"));
         onView(withId(R.id.spinner_ID_ReleaseAdKategorie)).perform(scrollTo(),click());
@@ -36,10 +38,22 @@ public class ReleaseAdInstrumentedTest {
         onView(withId(R.id.textView_ID_Preis)).perform(scrollTo(), typeText("1000"));
         onView(withId(R.id.spinner_ID_Einheit)).perform(scrollTo(),click());
         pressBack();
-        /*onView(withId(R.id.checkBox_ID_BiologischerAnbau)).perform(scrollTo(),click());
-        /*onView(withId(R.id.checkBox_ID_SelbstErnten)).perform(scrollTo(),click());
-        onView(withId(R.id.checkBox_ID_BereitsGeerntet)).perform(scrollTo(),click());*/
+        onView(withId(R.id.checkBox_ID_ReleaseAd_BiologischerAnbau)).perform(scrollTo(),click());
+        onView(withId(R.id.radioButton_ID_ReleaseAd_SelbstErnten)).perform(scrollTo(),click());
+        onView(withId(R.id.radioButton_ID_ReleaseAd_BereitsGeerntet)).perform(scrollTo(),click());
         onView(withId(R.id.button_ID_ReleaseAd_Abbrechen)).perform(scrollTo(),click());
         onView(withId(R.id.button_ID_ReleaseAdSpeichern)).perform(scrollTo(),click());
+    }
+
+    public void moveToRightAcitivity() {
+        onView(withId(R.id.textViewEmail)).perform(typeText("Test@gmx.at"));
+        onView(withId(R.id.textViewPassword)).perform(typeText("Test"));
+        closeSoftKeyboard();
+        onView(withId(R.id.buttonLogin)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
