45ec9ab07f93defa94370631144f38b4d9179578
kevintu95 <k.triebnig@student.tugraz.at>
2017-05-24 16:34:36 +0200

    [LH, KT] fixed LoginInstrumentedTest -- Wrong IDS

--- app/src/androidTest/java/at/sw2017xp3/regionalo/LoginInstrumentedTest.java
+++ app/src/androidTest/java/at/sw2017xp3/regionalo/LoginInstrumentedTest.java
@@ -58,7 +58,7 @@ public class LoginInstrumentedTest {
         onView(withId(R.id.textViewEmail)).perform(typeText("max.mustermann@gmusterx.at"));
         closeSoftKeyboard();
         onView(withId(R.id.buttonLogin)).perform(click());
-        onView(withId(R.id.textView_ID_LoginErrors)).check(matches(withText("Bitte Passwort eingeben!")));
+        onView(withId(R.id.textView_ID_LoginErrors)).check(matches(withText("Bitte Email und Passwort eingeben!")));
     }
 
     @Test
@@ -66,7 +66,7 @@ public class LoginInstrumentedTest {
         onView(withId(R.id.textViewPassword)).perform(typeText("max.mustermann@gmusterx.at"));
         closeSoftKeyboard();
         onView(withId(R.id.buttonLogin)).perform(click());
-        onView(withId(R.id.textView_ID_LoginErrors)).check(matches(withText("Bitte E-Mail eingeben!")));
+        onView(withId(R.id.textView_ID_LoginErrors)).check(matches(withText("Bitte Email und Passwort eingeben!")));
     }
 
     @Test
