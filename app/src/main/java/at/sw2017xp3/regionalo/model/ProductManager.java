70d946b169e257eacdd47d8dceed6166735e2dcc
kevintu95 <k.triebnig@student.tugraz.at>
2017-06-07 11:14:51 +0200

    [JS,KT] delete WriteLongLat

--- app/src/main/java/at/sw2017xp3/regionalo/model/ProductManager.java
+++ app/src/main/java/at/sw2017xp3/regionalo/model/ProductManager.java
@@ -69,9 +69,6 @@ public class ProductManager {
 
             p = JsonObjectMapper.CreateProduct(mJsonObject);
 
-            if (p.getUser().getLatitude() == 0d) {
-                Core.getInstance().getUsers().WriteLongLat(p.getUser());
-            }
 
             addProduct(p);
 
